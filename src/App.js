746cf15ea4b52688417cb263d2e30de282dbd947
Natalie Salemme <NatalieSalemme@gmail.com>
2018-09-17 20:47:19 -0700

    changing inputs to match tax calculator

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Mortgage Calculator</h1>
+        <h1>Sales Tax Calculator</h1>
         <Input />
       </div>
     );
--- src/Input.js
+++ src/Input.js
@@ -5,31 +5,19 @@ export class Input extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loan: '',
-      interest: '',
-      length: ''
+      price: '',
+      tax: ''
     }
   }
   render() {
     return (
       <div className="input-container">
-        <div classname="row">
-        <label className="labels" for="loan">Loan Amount: </label>
-        <input id="loan" type="text" placeholder="Loan Amount"/><br />
-        </div>
-        <div className="row">
-        <label className="labels" for="interest">Interest Rate: </label>
-        <input id="interest" type="text" placeholder="Interest Rate" /><br />
-      </div>
-      <div className="row">
-        <label className="labels" for="length">Length of loan: </label>
-        <input id="length" type="text" placeholder="Length of Loan" />
-      </div>
-      <br />
-      <br />
-      <br />
+
+        <label className="labels" for="price">Price: </label>
+        <input id="price" type="text" placeholder="Price"/><br />
+    
       <div>
-      <h3>Your estimated monthly payment will be:</h3>
+      <h3>Your total will be: </h3>
       <h4>$Results</h4>
     </div>
       </div>
