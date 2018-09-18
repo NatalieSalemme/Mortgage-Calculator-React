c8a8be73773b8a73423bfc8ab5770006948ce44d
Natalie Salemme <NatalieSalemme@gmail.com>
2018-09-17 21:09:11 -0700

    adding state functionality to price

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import { Input } from './Input';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    }
+    this.handleChange= this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+    console.log(e.target.value);
+  }
+  handleSubmit(e) {
+    alert(this.state.value);
+    e.preventDefault();
+  }
   render() {
     return (
-      <div className="App">
+      <form onSubmit={this.handleSubmit} className="container">
         <h1>Sales Tax Calculator</h1>
-        <Input />
+        <div className="input-container">
+        <label className="labels" for="price">Price: </label>
+        <input id="price" type="text" placeholder="Price" value={this.state.value} onChange={this.handleChange} />
+        <input id="submit" value="Submit" type="submit" />
+
+      <div>
+      <h3>Your total will be: </h3>
+      <h4>{this.state.value}</h4>
+    </div>
       </div>
+    </form>
     );
   }
 }
-
 export default App;
